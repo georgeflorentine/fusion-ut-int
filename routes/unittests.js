b5badf44c12a3e33b7569e453cc5e3636c3ed644
george florentine <george.florentine@fdiinc.com>
2018-10-25 17:12:07 -0600

    added code to process POST request  payload and populate note field of TC result

--- routes/unittests.js
+++ routes/unittests.js
@@ -15,12 +15,12 @@ router.get('/', function(req, res, next) {
     var rallyAPI = getAPI();
 
     // test -  get the list of top level folders in our test plan
-    getTestFolders(rallyAPI);
+    // getTestFolders(rallyAPI);
 
     // write the unit tests  results out to the appropriate test runs in Rally
     writeTestResults(rallyAPI);
 
-    res.send('unit tests POST method');
+    res.send('unit tests POST method succeeded!');
   });
 
   function getAPI() {
@@ -46,8 +46,8 @@ router.get('/', function(req, res, next) {
   function getTestFolders(rallyAPI) {
     var objectToRead= ('/testfolder/262051648568');
 
-    // make sure we  can find the root unit test folder. 
-    // HACK: this is a hard coded object id, probably should query for folder name
+
+    // NOTE: this is a hard coded object id, probably should query for folder name
     var results = 0;
     results = rallyAPI.get({
         ref: objectToRead, //may be a ref ('/defect/1234') or an object with a _ref property
@@ -62,24 +62,39 @@ router.get('/', function(req, res, next) {
             console.log(error);
         } else {
             console.log(result.Object);
-
-            // ok, we've got the root UnitTest folder. Look at the request object to determine what  folder we should put the test results into
-            var payload =  JSON.stringify(postReq.body);
-            console.log(" calling application submitted POST with body: " + payload);
         }
     });
   }
   
   function writeTestResults(restApi) {
+
+    // get the date in ISO 8601 format
+    var date = new Date();
+    var isoDate = date.toISOString(); 
+    var  note = new Object();
+    var unitTestResults = 'Fail'    ;
+
+    // DEBUG: dump the json we got in the POST request
+    //dumpJSON(postReq.body);
+
+    // NOTE: need to extract the test case name from the JSON data we received and then find the ID of the TC. 
+    // right now we're putting all  test results under one test case, which is  too course  grained. We
+    // like  to  associate a  group of unit test results  to  specific unit tesss so  we can see  what areas
+    // of the system have passing/failing unit  tests
+    var results = generateTestResults(postReq.body, note);
+    if (results) {
+        unitTestResults = 'Pass';
+    }
+
     restApi.create({
         type: 'testcaseresult', //the type to create
         data: {
             Name: 'UnitTestIntegrationTestResult', //the data with which to populate the new object
             TestCase: 'https://rally1.rallydev.com/slm/webservice/v2.0/testcase/260569786444',
-            Verdict: 'Pass',
-            Date: '2018-10-24',
+            Verdict: unitTestResults,
+            Date: isoDate,
             Build: 'integration testing',
-            Notes: 'utint2'
+            Notes: note.results
         },
         fetch: ['FormattedID'],  //the fields to be returned on the created object
         scope: {
@@ -94,4 +109,32 @@ router.get('/', function(req, res, next) {
         }
     });
   }
+  function dumpJSON(unitTestResultsJSON, noteResult) {
+    for(var exKey in unitTestResultsJSON) {
+        console.log("key:" + exKey +", value:" + unitTestResultsJSON[exKey]);
+        }
+    }
+    function generateTestResults(unitTestResultsJSON, noteResult) {
+        var  results = '';
+        var pass = false;
+
+        // HACK: this is  brittle, need  a better  way of determing what  info  we  use from  the test result
+        var i = 0;
+        for (exKey in unitTestResultsJSON) {
+            results  += exKey + ": " + unitTestResultsJSON[exKey] + ', ';
+            if (exKey == 'success') {
+                if  (unitTestResultsJSON[exKey]) {
+                    pass = true;
+                }
+            }
+            // only need the first 11 items
+            if (i == 11) {
+                break;
+            }
+            i++;
+        }
+        noteResult.results = results;
+        return pass;
+    }
+  
   module.exports = router;
